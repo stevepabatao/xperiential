0576bf17f5240afdbaa860dc666a5529078383c9
Steve Pabatao <s.pabatao@digittie.com>
2021-04-26 18:08:48 +0800

    updated some formatting and layout

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let text1, text2;
 
-text1 ="<h4>This is a sample text 1</h4><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id quam sapiente molestiae numquam quas, voluptates omnis nulla ea odio quia similique corrupti magnam, doloremque laborum.   </p>"; 
+text1 ="<h3>This is a sample text 1</h3><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id quam sapiente molestiae numquam quas, voluptates omnis nulla ea odio quia similique corrupti magnam, doloremque laborum.   </p>"; 
 text2 ="<h3>This is a sample text 2</h3><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id quam sapiente molestiae numquam quas, voluptates omnis nulla ea odio quia similique corrupti magnam, doloremque laborum.   </p>"; 
 text3 ="<h3>This is a sample text 3</h3><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id quam sapiente molestiae numquam quas, voluptates omnis nulla ea odio quia similique corrupti magnam, doloremque laborum.   </p>"; 
 text4 ="<h3>This is a sample text 4</h3><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id quam sapiente molestiae numquam quas, voluptates omnis nulla ea odio quia similique corrupti magnam, doloremque laborum.   </p>"; 
@@ -17,7 +17,7 @@ $(".circle li:nth-child(2)").mouseover(function(){
 });
 
 $(".circle li:nth-child(3)").mouseover(function(){
-    $('.box').html(text4);
+    $('.box').html(text3);
 });
 
 $(".circle li:nth-child(4)").mouseover(function(){
